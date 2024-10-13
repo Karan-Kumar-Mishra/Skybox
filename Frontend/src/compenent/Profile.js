44a2590de9f4ba8d1522a3605428ae3de429ae65
Karan-Kumar-Mishra <emiltokaranmishra@gmail.com>
2024-10-13 20:21:09 +0530

    change the some file

--- Frontend/src/compenent/Profile.js
+++ Frontend/src/compenent/Profile.js
@@ -1,17 +1,16 @@
 import React, { useEffect } from "react";
-import DefaultUser from './Images/DefaultUser.png'
+import DefaultUser from "./Images/DefaultUser.png";
 import { useAuth0 } from "@auth0/auth0-react";
 export default function Profile() {
-  const { isAuthenticated,user } = useAuth0();
-  useEffect(()=>{
-    if(isAuthenticated)
-    {
-      DefaultUser=user.picture
-      console.log("is authenticated",user.picture,DefaultUser)
+  const { isAuthenticated, user } = useAuth0();
+  useEffect(() => {
+    if (isAuthenticated) {
+      DefaultUser = user.picture;
+      console.log("is authenticated", user.picture, DefaultUser);
     }
-  },[user])
+  }, [user]);
   return (
-    <div className="h-screen w-screen bg-gradient-to-r from-black to-indigo-900 flex items-center justify-center flex-col  ">
+    <div className="h-screen bg-gradient-to-r from-black to-indigo-900 flex items-center justify-center flex-col  ">
       <div className="images  ">
         <img
           src={DefaultUser}
@@ -20,12 +19,13 @@ export default function Profile() {
         />
       </div>
       <div className="content m-2 flex-col justify-center gap-2 content-center text-center text-white font-bold">
-       
-        <h1>{user.name}</h1> 
-        <h1>{user.email}</h1> 
-          <div className="buttons m-4">
-            <button className="border-solid p-3 rounded-3xl bg-gradient-to-r from-black to-indigo-900 shadow-lg shadow-black" >Delete Account</button>
-          </div>
+        <h1>{user.name}</h1>
+        <h1>{user.email}</h1>
+        <div className="buttons m-4">
+          <button className="border-solid p-3 rounded-3xl bg-gradient-to-r from-black to-indigo-900 shadow-lg shadow-black">
+            Delete Account
+          </button>
+        </div>
       </div>
     </div>
   );
