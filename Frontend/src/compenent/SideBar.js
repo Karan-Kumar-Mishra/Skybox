511a493dc58a6c7bcf6292dcd1dc3b70c0e7fd71
Karan-Kumar-Mishra <emiltokaranmishra@gmail.com>
2024-11-22 20:31:30 +0530

    adding dashboard sub components

--- Frontend/src/compenent/SideBar.js
+++ Frontend/src/compenent/SideBar.js
@@ -1,8 +1,8 @@
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
 export default function SideBar() {
+  const navigate=useNavigate();
   return (
-    
     <div className="h-screen bg-slate-800 p-5  ">
       <ul className="flex flex-col gap-6">
         <li className="flex-center cursor-pointer p-16-semibold  whitespace-nowrap">
@@ -16,7 +16,8 @@ export default function SideBar() {
               fill="#000000"
             >
               <g strokeWidth="0" id="SVGRepo_bgCarrier"></g>
-              <div                 strokeLinejoin="round"
+              <div
+                strokeLinejoin="round"
                 strokeLinecap="round"
                 id="SVGRepo_tracerCarrier"
               ></div>
@@ -29,8 +30,25 @@ export default function SideBar() {
             Notes
           </button>
         </li>
-        <li className="flex-center cursor-pointer p-16-semibold w-full whitespace-nowrap">
+        <li className="flex-center cursor-pointer p-16-semibold  whitespace-nowrap focus:decoration-white">
           <button className="p-16-semibold flex  gap-4 p-4 group font-semibold rounded-full bg-cover hover:bg-purple-100 hover:shadow-inner hover:text-black focus:bg-gradient-to-r from-black to-indigo-900 focus:text-white text-white transition-all ease-linear">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="icon glyph size-6 group-focus:fill-white group-focus:stroke-white"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+              fill="#0000F5"
+            >
+              <path d="M240-400h320v-80H240v80Zm0-120h480v-80H240v80Zm0-120h480v-4q-37-8-67.5-27.5T600-720H240v80ZM80-80v-720q0-33 23.5-56.5T160-880h404q-4 20-4 40t4 40H160v525l46-45h594v-324q23-5 43-13.5t37-22.5v360q0 33-23.5 56.5T800-240H240L80-80Zm80-720v480-480Zm600 80q-50 0-85-35t-35-85q0-50 35-85t85-35q50 0 85 35t35 85q0 50-35 85t-85 35Z" />
+            </svg>
+            Notification
+          </button>
+        </li>
+        <li className="flex-center cursor-pointer p-16-semibold w-full whitespace-nowrap">
+          <button
+           onClick={navigate('/settings')} 
+          className="p-16-semibold flex  gap-4 p-4 group font-semibold rounded-full bg-cover hover:bg-purple-100 hover:shadow-inner hover:text-black focus:bg-gradient-to-r from-black to-indigo-900 focus:text-white text-white transition-all ease-linear">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
